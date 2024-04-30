8d308fc068b359c09e60120fabb9226e6694af31
Brian Rivera <69652100+BraitoRX@users.noreply.github.com>
2024-04-30 13:50:18 -0500

    Corregir manejo de archivos en file_manager.html y views.py

--- home/views.py
+++ home/views.py
@@ -172,7 +172,7 @@ def file_manager(request, file_path=None):
                 hdfs.get(archivo['file'], absolute_file_path)
                 relative_file_path = os.path.join('Temp', local_file_name)
                 archivo['temp'] = relative_file_path
-        print(directorios)
+            
         return render(request, 'pages/file-manager.html', {'directories': directorios,'selected_directory': "/",'page_obj': page_obj,'segment': 'file_manager'})
     else:
         normalized_file_path = file_path.replace('%slash%', '/')
