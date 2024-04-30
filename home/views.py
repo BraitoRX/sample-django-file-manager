04492ef7a6cc4563f123ab0c98c831cc88acdea9
Brian Rivera <69652100+BraitoRX@users.noreply.github.com>
2024-04-30 14:58:27 -0500

    Corregir errores de visualización en file-manager.html y views.py

--- home/views.py
+++ home/views.py
@@ -160,6 +160,7 @@ def organizar_directorios_archivos(archivos, directorios, hdfs, temp_dir_path, r
             hdfs.get(archivo['file'], absolute_file_path)
             relative_file_path = os.path.join('Temp', local_file_name)
             archivo['temp'] = relative_file_path
+            
 
     paginator_dir = Paginator(directorios, 5)
     paginator_page_number_dir = request.GET.get('page_dir', 1)  
