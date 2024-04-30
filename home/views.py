4757e0ad4c0830a6d22294d68cbeafc7371bed0b
Brian Rivera <69652100+BraitoRX@users.noreply.github.com>
2024-04-30 14:11:58 -0500

    Corregir paginación y manejo de archivos en file_manager.html y views.py

--- home/views.py
+++ home/views.py
@@ -147,6 +147,11 @@ def get_breadcrumbs(request):
     return breadcrumbs
 
 def organizar_directorios_archivos(archivos, directorios, hdfs, temp_dir_path, request):
+
+    paginator = Paginator(archivos, 10)
+    page_number = request.GET.get('page', 1)  # Obtiene el número de página de GET request
+    page_obj = paginator.get_page(page_number)  # Obtiene los objetos para la página actual
+
     for archivo in archivos:
         if archivo['file_extension'] in [".png",".jpg",".jpeg",".gif"]:
             local_file_name = os.path.basename(archivo['file'])
@@ -163,7 +168,7 @@ def organizar_directorios_archivos(archivos, directorios, hdfs, temp_dir_path, r
     for directorio in directorios:
         path = os.path.join("/", directorio['name'])
         directorio['path'] = path
-    return archivos, directorios, page_obj_dir
+    return archivos, directorios, page_obj_dir, page_obj
 
 
 def file_manager(request, file_path=None):
@@ -177,7 +182,7 @@ def file_manager(request, file_path=None):
     # Si no hay una ruta de archivo especificada, muestra el directorio raíz
     if file_path is None:
         archivos, directorios = get_files_from_directory_hdfs(hdfs, "/")
-        archivos, directorios, page_obj_dir = organizar_directorios_archivos(archivos, directorios, hdfs, temp_dir_path, request)
+        archivos, directorios, page_obj_dir,page_obj = organizar_directorios_archivos(archivos, directorios, hdfs, temp_dir_path, request)
 
         return render(request, 'pages/file-manager.html', {'directories': directorios,"page_obj_dir":page_obj_dir,'selected_directory': "/",'page_obj': page_obj,'segment': 'file_manager'})
     else:
