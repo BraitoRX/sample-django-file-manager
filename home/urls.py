a26d3671b31a60782a5d0e7253089391d87ceefb
Brian Rivera <69652100+BraitoRX@users.noreply.github.com>
2024-04-30 16:03:42 -0500

    Corregir enlace y favicon en home/urls.py

--- home/urls.py
+++ home/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('file-manager/', views.file_manager, name='file_manager'),
-    path('file-detail/<path:file_path>/', views.file_detail, name='file_detail'),
-    re_path(r'^file-manager/(?P<directory>.*)?/$', views.file_manager, name='file_manager'),
+    path('file-manager/<path:file_path>/', views.file_manager, name='file_detail'),
     path('delete-file/<str:file_path>/', views.delete_file, name='delete_file'),
     path('download-file/<str:file_path>/', views.download_file, name='download_file'),
     path('upload-file/', views.upload_file, name='upload_file'),
