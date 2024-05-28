70e80db54d2d9e5d509fa43c1edb09a89978a4da
Brian Rivera <69652100+BraitoRX@users.noreply.github.com>
2024-05-28 01:53:27 -0500

    xd

--- home/views.py
+++ home/views.py
@@ -393,6 +393,16 @@ def file_detail(request, file_path=None):
 def view_selected_files(request):
     if request.method == 'POST':
         selected_files = request.POST.getlist('selected_files')
-        return render(request, 'pages/view_selected_files.html', {'selected_files': selected_files})
+        file_details = []
+        for file_path in selected_files:
+            file_name = file_path.split('/')[-1]
+            file_extension = file_name.split('.')[-1].upper()
+            file_details.append({
+                'path': file_path,
+                'name': file_name,
+                'extension': file_extension
+            })
+        return render(request, 'pages/view_selected_files.html', {'file_details': file_details})
     else:
         return redirect('file_manager')
+
