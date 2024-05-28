38fe33b07994b402b3f33a737e81d30153189201
Brian Rivera <69652100+BraitoRX@users.noreply.github.com>
2024-05-28 00:58:02 -0500

    Actualizar ruta de la plantilla para mostrar archivos seleccionados

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, re_path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('file-detail/', views.file_manager, name='file_manager'),
     path('file-detail/<path:file_path>/', views.file_manager, name='file_detail'),
-    re_path(r'^file-detail/(?P<directory>.*)?/$', views.file_manager, name='file_manager'),
+    re_path(r'^file-detail/(?P<directory>.*)?/$', views.file_manager, name='file_manager_directory'),
     path('delete-file/<str:file_path>/', views.delete_file, name='delete_file'),
     path('download-file/<str:file_path>/', views.download_file, name='download_file'),
     path('upload-file/', views.upload_file, name='upload_file'),
