2f111957a77127ecdee85157b24e2f53f6794cd1
Brian Rivera <69652100+BraitoRX@users.noreply.github.com>
2024-05-28 01:33:20 -0500

    Actualizar vista para ver archivos seleccionados y ajustar ruta de la plantilla

--- home/views.py
+++ home/views.py
@@ -393,7 +393,6 @@ def file_detail(request, file_path=None):
 def view_selected_files(request):
     if request.method == 'POST':
         selected_files = request.POST.getlist('selected_files')
-        return render(request, 'pages/view_selected_files.html', {'selected_files': selected_files})
+        return render(request, 'view_selected_files.html', {'selected_files': selected_files})
     else:
         return redirect('file_manager')
-
