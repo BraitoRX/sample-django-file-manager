e54658b6cfb7a6577fbcf453ad18a91a6256700a
Brian Rivera <69652100+BraitoRX@users.noreply.github.com>
2024-05-28 02:52:58 -0500

    xd

--- home/views.py
+++ home/views.py
@@ -352,8 +352,10 @@ def upload_file(request):
 def file_detail(request, file_path=None):
     if file_path is None:
         archivos = get_files_from_directory(settings.MEDIA_ROOT)
-        return render(request, 'pages/file_detail.html', {'files': archivos, 'selected_files': request.session.get('selected_files', [])})
-    
+        return render(request, 'pages/file_detail.html', {
+            'files': archivos,
+            'selected_files': request.session.get('selected_files', [])
+        })
     else:
         file_path = file_path.replace('%slash%', '/')
 
@@ -373,19 +375,21 @@ def file_detail(request, file_path=None):
             csv_text = ''
             if file_name.endswith('.csv'):
                 csv_text = convert_csv_to_text(absolute_file_path)
-                print(' > csv_text ' + csv_text)
 
-            return render(request, 'pages/file_detail.html', {'file_path': file_path, 'file_name': file_name, 'csv_text': csv_text, 'file_extension': os.path.splitext(file_name)[1],'selected_files': request.session.get('selected_files', [])})
-
+            return render(request, 'pages/file_detail.html', {
+                'file_path': file_path,
+                'file_name': file_name,
+                'csv_text': csv_text,
+                'file_extension': os.path.splitext(file_name)[1],
+                'selected_files': request.session.get('selected_files', [])
+            })
         
         if os.path.isdir(absolute_file_path):
-            
-            # Convertir las rutas relativas de los archivos en rutas absolutas
             absolute_files = get_files_from_directory(absolute_file_path)
-            print(' > files_in_folder ' + str(absolute_files))
-
-        # Renderiza la plantilla para mostrar la lista de archivos en la carpeta
-        return render(request, 'pages/file_detail.html', {'files': absolute_files})
+            return render(request, 'pages/file_detail.html', {
+                'files': absolute_files,
+                'selected_files': request.session.get('selected_files', [])
+            })
 
 
 
@@ -396,6 +400,7 @@ def view_selected_files(request):
         if 'selected_files' not in request.session:
             request.session['selected_files'] = []
         # Combina las listas y elimina duplicados
+        print(' > selected_files ' + str(selected_files))
         request.session['selected_files'] = list(set(request.session['selected_files'] + selected_files))
         request.session.modified = True  # Marca la sesión como modificada para guardar los cambios
         
