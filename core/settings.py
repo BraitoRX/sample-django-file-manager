8fca4d510a8ab81fc2608d735596585d76eedb9f
Brian Rivera <69652100+BraitoRX@users.noreply.github.com>
2024-05-28 03:35:09 -0500

    xd

--- core/settings.py
+++ core/settings.py
@@ -179,8 +179,8 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': 'media/Temp/',
-        'TIMEOUT': 20,  
+        'LOCATION': BASE_DIR / 'media/Temp/',
+        'TIMEOUT': 86400,  # 24 horas en segundos
         'OPTIONS': {
             'MAX_ENTRIES': 1000  # Limita el número de entradas en la caché a 1000
         }
