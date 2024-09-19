ef00386c741402f3e8354e02c37086fb9f4e0a63
Brian <rbrina4@gmail.com>
2024-09-19 10:48:28 -0500

    xd

--- home/views.py
+++ home/views.py
@@ -230,7 +230,7 @@ def file_manager(request, file_path=None, directory=None):
 
             try:
                 # Procesa el archivo dependiendo de su tipo
-                if file_extension in ['.txt', '.csv', '.png', '.mp4', '.wav','.opus', ".jpg", ".jpeg"]:
+                if file_extension in ['.txt', '.csv', '.png', '.mp4','.mp3' ,'.wav','.opus', ".jpg", ".jpeg"]:
                     return render(request, 'pages/file-manager.html', {
                         'file_path': normalized_file_path,
                         'temp': relative_file_path,
